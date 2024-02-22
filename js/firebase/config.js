879718e66e57834cb62d4a7493b1b2eac3f97cfb
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-02-22 15:58:21 +0300

    Add files via upload

---
+++ js/firebase/config.js
@@ -0,0 +1,21 @@
+// Your web app's Firebase configuration
+
+//var firebaseConfig = {
+ //   apiKey: "AIzaSyDhrH8WLEB_H1BNl6jrgKdf4mvgdsff-Zw",
+  //  authDomain: "social-network-1077.firebaseapp.com",
+  //  databaseURL: "https://social-network-1077.firebaseio.com",
+  //  projectId: "social-network-1077",
+  //  storageBucket: "social-network-1077.appspot.com",
+  //  messagingSenderId: "729586005355",
+  //  appId: "1:729586005355:web:44cfb3f382887095fc082e"
+//};
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDAe_Po-fZxpWOQJ1Xw9y9eHWy1WBjOfo4",
+    authDomain: "dkont-9f30d.firebaseapp.com",
+    projectId: "dkont-9f30d",
+    storageBucket: "dkont-9f30d.appspot.com",
+    messagingSenderId: "722093829050",
+    appId: "1:722093829050:web:a9fa8d3857843989b40b83"
+  };
