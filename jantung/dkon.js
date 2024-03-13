03d4282c3afff477c1e1bafb68cb06384ecef7bd
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-03-13 12:46:25 +0300

    Update dkon.js

--- jantung/dkon.js
+++ jantung/dkon.js
@@ -2,10 +2,10 @@
 
 importScripts('https://res.dkon.app/jantung/worker.js');
 
-toolbox.precache(["index.html","style/style.css"]);
+toolbox.precache(["https://dkon.app/?index.html","https://dkon.app/style/style.css"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('https://dkon.app/images/*', toolbox.cacheFirst);
 
-toolbox.router.get('/*', toolbox.networkFirst, {
+toolbox.router.get('https://dkon.app/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
