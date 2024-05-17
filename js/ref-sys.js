e2bf928d0bc40f0c85998f9d60476e30b735e055
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-05-17 12:05:29 +0300

    Update ref-sys.js

--- js/ref-sys.js
+++ js/ref-sys.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let params = new URLSearchParams(document.location.search.substring(1));
     let qstring = params.get("referrer"); 
     let results = document.cookie.match(/referrer=(.+?)(;|$)/);
-    document.getElementById("referrer").value = results[1];
+  
   
 switch (qstring) {
   case null:
