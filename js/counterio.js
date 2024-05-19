6765ad7dc6ab69622db7c5aaea86782ac4985eaa
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-05-19 19:50:07 +0300

    Update counterio.js

--- js/counterio.js
+++ js/counterio.js
@@ -12,7 +12,8 @@ $(document).ready(() => {
 const updatePlayercount = (ip, port) => {
     $.get(`https://dkon.app/api/v5.7/method/dkon.stats.posts?id=${ip}&${port}`, (result) => {
         if (result.hasOwnProperty('posts')) {
-           $(".dkonstats").html(parseFloat(result.posts).toFixed(0)); // 0 ->2
+        //   $(".dkonstats").html(parseFloat(result.posts).toFixed(0)); // 0 ->2
+           $(".dkonstats").html(result.posts);  
         } else {
            $(".dkonstatss").html("Server isn't online!");
   
