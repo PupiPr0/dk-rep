1a3424e7beabb23ffa38c6efad48ba14d497b304
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-05-19 19:34:18 +0300

    Create counterio.js

---
+++ js/counterio.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+    let ip = $(".dkonstats").attr("data-ip");
+    let port = $(".dkonstats").attr("data-port");
+    if (port == "" || port == null) port = "443";
+    if (ip == "" || ip == null) return console.error("Error ");
+    updatePlayercount(ip, port);
+    setInterval(() => {
+        updatePlayercount(ip, port);
+    }, 7000);
+});
+
+const updatePlayercount = (ip, port) => {
+    $.get(`https://dkon.app/api/v5.7/method/dkon.stats.posts?id=${ip}&${port}`, (result) => {
+        if (result.hasOwnProperty('posts')) {
+           $(".dkonstats").html(parseFloat(result.price).toFixed(2));
+        } else {
+           $(".dkonstatss").html("Server isn't online!");
+  
+        }
+    });
+};
+
+!function(a) {
+  }(jQuery);
