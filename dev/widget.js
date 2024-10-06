5857e6df664892da39c7fafd6087bf6e5c0aca95
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-10-06 16:05:46 +0300

    Create widget.js

---
+++ dev/widget.js
@@ -0,0 +1,38 @@
+(function() {
+    const script = document.currentScript;
+
+    function createIframe(src, width, height, border) {
+        const iframe = document.createElement('iframe');
+        iframe.src = src;
+        iframe.style.width = width;
+        iframe.style.height = height;
+        iframe.style.border = border;
+        iframe.setAttribute('scrolling', 'no');
+        iframe.setAttribute('marginheight', '0');
+        iframe.setAttribute('marginwidth', '0');
+        return iframe;
+    }
+
+    const widgetName = script.getAttribute('dkon-widget');
+    if (widgetName) {
+        const iframe = createIframe(`https://dkon.app/dev/generate.widget.community?group=${widgetName}`, '350px', '400px', '0px #ffffff none');
+        script.parentNode.insertBefore(iframe, script);
+    }
+
+    const postData = script.getAttribute('data-dkon-post');
+    if (postData) {
+        const width = script.getAttribute('data-width') || '100%';
+        const height = script.getAttribute('data-height') || '500px'; 
+        const [name, id] = postData.split('/');
+
+        const widgetContainer = document.createElement('div');
+        widgetContainer.style.width = width;
+        widgetContainer.style.height = height; 
+        widgetContainer.style.overflow = 'hidden'; 
+
+        const iframe = createIframe(`https://dkon.app/${name}/post/${id}/embed_widget`, '100%', '100%', 'none');
+        widgetContainer.appendChild(iframe);
+
+        script.parentNode.insertBefore(widgetContainer, script);
+    }
+})();
