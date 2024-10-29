d650a9ef1f46b1ddc73232bea2119357c73caf89
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-10-29 13:34:07 +0300

    Create start.js

---
+++ packed/language/dktemp/v3/blue/json/start.js
@@ -0,0 +1,27 @@
+function loadLanguage(lang) {
+        fetch(`http://res.dkon.app/packed/language/dktemp/v3/blue/json/${lang}.json`)  
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('page-title').innerText = data.title;
+                document.querySelector('meta[name="description"]').setAttribute("content", data.description);
+                document.getElementById('header-h1').innerText = data.h1;
+                document.getElementById('header-h2').innerHTML = data.h2;
+                document.getElementById('paragraph-1').innerHTML = data.p1;
+                document.getElementById('paragraph-2').innerHTML = data.p2;
+                document.getElementById('paragraph-3').innerHTML = data.p3;
+                document.getElementById('footer-text').innerHTML = data.footer;
+            })
+            .catch(error => console.error('Error loading language file:', error));
+    }
+    const userLang = navigator.language || navigator.userLanguage; 
+    const lang = userLang.startsWith('ru') ? 'ru' :
+                 userLang.startsWith('pl') ? 'pl' :
+                 userLang.startsWith('id') ? 'id' :
+                 userLang.startsWith('uk') ? 'uk' :
+                 userLang.startsWith('ar') ? 'ar' :
+                 userLang.startsWith('ur') ? 'pk' : 
+                 userLang.startsWith('hi') ? 'hi' :
+                 userLang.startsWith('pt-BR') ? 'pt-br' : 
+                 'en';  
+    loadLanguage(lang);
+    loadLanguage(lang);
