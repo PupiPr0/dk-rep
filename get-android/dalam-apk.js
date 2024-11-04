68696e267b3ffefc2d12e95b3c69c5c4c25e6478
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-11-04 14:49:50 +0300

    Update dalam-apk.js

--- get-android/dalam-apk.js
+++ get-android/dalam-apk.js
@@ -1,9 +1,7 @@
-const container = document.querySelector('.screenshot-container');
+/*const container = document.querySelector('.screenshot-container');
 
 let scrollAmount = 0;
-
-//const scrollStep = 1; 
-const scrollStep = 2; 
+const scrollStep = 1; 
 
 
 function autoScroll() {
@@ -15,6 +13,40 @@ function autoScroll() {
         container.scrollTo(0, 0); 
     }
 }
+setInterval(autoScroll, 50);
+*/
+const container = document.querySelector('.screenshot-container');
+const images = document.querySelectorAll('.screenshot');
 
-//setInterval(autoScroll, 50);
-setInterval(autoScroll, 100); 
+let scrollAmount = 0;
+const scrollStep = 2; 
+let imagesLoaded = 0;
+
+function autoScroll() {
+    if (scrollAmount < container.scrollWidth - container.clientWidth) {
+        container.scrollBy(scrollStep, 0);
+        scrollAmount += scrollStep;
+    } else {
+        scrollAmount = 0; 
+        container.scrollTo(0, 0); 
+    }
+}
+
+images.forEach((img) => {
+    img.onload = () => {
+        imagesLoaded++;
+        if (imagesLoaded === images.length) {
+            setInterval(autoScroll, 100); // Запускаем авто-прокрутку
+        }
+    };
+    
+    img.onerror = () => {
+        console.error(`Ошибка загрузки изображения: ${img.src}`);
+    };
+});
+
+images.forEach((img) => {
+    if (img.complete) {
+        imagesLoaded++;
+    }
+});
