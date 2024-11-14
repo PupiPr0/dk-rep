23093889ec2c0b195bddc1f1d963bcc16800f6c4
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-11-15 01:38:08 +0300

    Update dalam-apk.js

--- get-android/dalam-apk.js
+++ get-android/dalam-apk.js
@@ -1,5 +1,47 @@
 //Dkon.app Script
 
+function getOS() {
+    const userAgent = window.navigator.userAgent;
+
+    if (/Windows NT 10.0/.test(userAgent) || /Windows NT 11.0/.test(userAgent)) {
+        return 'Windows';
+    } else if (/Linux/.test(userAgent)) {
+        return 'Linux';
+    } else if (/Android/i.test(userAgent)) {
+        return 'Android';
+    }
+    return 'Unknown';
+}
+
+function downloadFile(url, filename) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename; 
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+window.onload = function() {
+    const os = getOS();
+
+    if (os === 'Windows') {
+        setTimeout(() => {
+            downloadFile('https://dkon.app/dev/last_version/dkon.exe', 'Dkon.exe');
+        }, 1000);
+    } else if (os === 'Linux') {
+        setTimeout(() => {
+            downloadFile('https://dkon.app/dev/last_version/dkon.appimage', 'Dkon.appimage');
+        }, 1000);
+    } else if (os === 'Android') {
+        setTimeout(() => {
+            downloadFile('https://dkon.app/dev/last_version/dkon.apk', 'Dkon.apk');
+        }, 1000);
+    }
+};
+
+
+/*
         function isAndroid() {
             return /Android/i.test(navigator.userAgent);
         }
@@ -19,6 +61,9 @@
                 setTimeout(downloadFile, 1000);
             }
         };
+
+        */
+
 
 /*const container = document.querySelector('.screenshot-container');
 
