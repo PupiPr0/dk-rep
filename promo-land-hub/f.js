4e8a826b109ace81172c624aa1e7c5e34aee66de
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2024-12-23 22:13:21 +0300

    Update f.js

--- promo-land-hub/f.js
+++ promo-land-hub/f.js
@@ -1,7 +1,7 @@
 //f.js footer
 const currentDomain = window.location.hostname;
 
-if (currentDomain === 'ukropenweb.github.io' || currentDomain === 'ukrop11enweb.github.io') {
+if (currentDomain === 'ukropenweb.github.io' || currentDomain === 'googlesocialnetwork.github.io') {
     window.location.href = 'https://dkon.app/?ref';
 }
 
