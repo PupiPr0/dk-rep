a1293e782c544d442f4788dc972a62f514218492
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2025-01-03 13:23:43 +0300

    Create land-cnf.js

---
+++ 0conf/land-cnf.js
@@ -0,0 +1,5 @@
+const currentDomain = window.location.hostname;
+
+if (currentDomain === 'ukropenweb.github.io' || currentDomain === 'googlesocialnetwork.github.io') {
+    window.location.href = 'https://dkon.app/?ref';
+}
