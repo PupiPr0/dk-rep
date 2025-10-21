c49da1b759d7a07f901eef94864dfa598d5e46b2
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2025-10-21 12:03:24 +0300

    Update th-v4.js

--- widget-style/th-v4.js
+++ widget-style/th-v4.js
@@ -1,5 +1,5 @@
 (function(){
-  const ALLOWED = new Set(['dark','solar','sepia','midnight','pink','grey','green','new-year','halloween','summer','marine']);
+  const ALLOWED = new Set(['dark','solar','sepia','midnight','pink','grey','green','newyear','halloween','summer','marine']);
 
   function getColorParam(){
     const params = new URLSearchParams(location.search);
