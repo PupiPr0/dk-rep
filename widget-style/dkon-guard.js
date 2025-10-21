12de8770b47665f99430c099a476f34a13f5e2ec
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2025-10-21 11:38:33 +0300

    Create dkon-guard.js

---
+++ widget-style/dkon-guard.js
@@ -0,0 +1,14 @@
+if (window.self === window.top) {
+    const currentUrl = window.location.href;
+    if (currentUrl.includes('/embed_widget')) {
+        const redirectUrl = currentUrl.replace('/embed_widget', '');
+        window.location.href = redirectUrl;
+    }
+}
+if (window.self === window.top) {
+    const currentUrl = window.location.href;
+    if (currentUrl.includes('/embed_widget')) {
+        const redirectUrl = currentUrl.replace('/embed_widget', '');
+        window.location.href = redirectUrl;
+    }
+}
