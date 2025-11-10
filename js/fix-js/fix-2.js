52b47b00b8b95e2b24de041b64930084f389801c
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2025-11-10 12:46:28 +0300

    Create fix-2.js

---
+++ js/fix-js/fix-2.js
@@ -0,0 +1,12 @@
+// Perbaiki tautan di komentar. 10 11 2025
+function makeLinksClickable() {
+            const mediaContent = document.querySelectorAll('.media-content');
+
+            mediaContent.forEach(element => {
+                const html = element.innerHTML;
+                const regex = /https?:\/\/[^\s]+/g; 
+                const newHtml = html.replace(regex, (url) => `<a href="${url}" target="_blank" rel="nofollow">${url}</a>`);
+                element.innerHTML = newHtml;
+            });
+        }
+        makeLinksClickable();
