69facd82d73ce711300c9960188ce0286c56f199
Ryszard Kwiatkowski <140899316+PupiPr0@users.noreply.github.com>
2025-11-28 13:46:01 +0300

    Create country.js

---
+++ ext/myip/lang/country.js
@@ -0,0 +1,31 @@
+// https://res.dkon.app/ext/myip/lang/country.js
+window.DKON_COUNTRIES = {
+ 
+
+  by: "Belarus",
+  ru: "Russia",
+  ua: "Ukraine",
+  kz: "Kazakhstan",
+  us: "United States",
+  de: "Germany",
+  pl: "Poland",
+  fr: "France",
+  gb: "United Kingdom",
+  es: "Spain",
+  it: "Italy",
+  nl: "Netherlands",
+  tr: "Türkiye",
+  id: "Indonesia",
+  vn: "Vietnam",
+  th: "Thailand",
+  my: "Malaysia",
+  ph: "Philippines",
+  br: "Brazil",
+  ca: "Canada",
+  au: "Australia",
+  jp: "Japan",
+  kr: "South Korea",
+  cn: "China",
+
+ 
+};
